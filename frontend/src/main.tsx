b6fda04a8d4e82adb32df3c6dd76f678b0cc2a22
Nicholas Tate <98060238+Kinny-B@users.noreply.github.com>
2025-09-07 02:25:50 -0500

    home page stuff
    added button links

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import "./index.css"; // Tailwind + global styles
+
+// React Query (for fetching from backend APIs)
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
