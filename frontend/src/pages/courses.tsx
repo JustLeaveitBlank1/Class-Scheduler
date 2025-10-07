daaf2fc8f6fc687474f34b2ecb5e247f476a9dad
Nicholas Tate <98060238+Kinny-B@users.noreply.github.com>
2025-10-07 01:01:29 -0500

    change list to table
    nice table

--- frontend/src/pages/courses.tsx
+++ frontend/src/pages/courses.tsx
@@ -20,29 +20,40 @@ const CoursesPage = () => {
     return (
         <div>
             <h1>All Courses</h1>
-            <div className='sqr'>
-                {courses ? (
-                    courses?.map((course) => {
-                        return (
-                            <ul>
-                                <li 
-                                    key={course.id}
-                                >
-                                    <h2>
-                                        {course.name}
-                                        {course.code}
-                                    </h2>
-                                    <p>
-                                        credits: {course.credit_hours}
-                                        time: {course.contact_hours}
-                                    </p>
-                                </li>
-                            </ul>
-                        );
-                    })
-                ) : (
-                    <div>Loading</div>
-                )}
+            <div className='box'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name&emsp;</th>
+                            <th>Code&emsp;</th>
+                            <th>Credits&emsp;</th>
+                            <th>Meeting Time &emsp;</th>  
+                        </tr>
+                    </thead>
+                </table>
+                <tbody className="divide-y divide-gray-700 bg-gray-900 text-gray-100">
+                    {courses ? (
+                        courses?.map((course) => {
+                            return (
+                                    <tr key={course.id} className="hover:bg-gray-800 transition-colors">  
+                                        <td className='px-4 py-2'>
+                                            {course.name}&ensp;
+                                        </td>
+                                        <td className='px-4 py-2'>
+                                            &emsp;{course.code}&emsp;
+                                        </td>
+                                        <td className="px-4 py-2 text-center">
+                                            &emsp;{course.credit_hours}&emsp;
+                                        </td>
+                                        <td className="px-4 py-2 text-center">
+                                            &emsp;{course.contact_hours}&emsp;
+                                        </td>
+                                    </tr>
+                            );
+                        })
+                    ) : (<div>Loading</div>)}   
+                </tbody>
+                
             </div>
         </div>
     );
